1de3e516de5fd0598d2bcddcda282171f8bac0f0
Ross Nomann <rossnomann@protonmail.com>
2020-03-29 15:15:43 +0400

    Impl Display for Subtitle

--- src/subtitle.rs
+++ src/subtitle.rs
@@ -1,4 +1,5 @@
 use crate::time::Time;
+use std::fmt;
 
 /// A subtitle item
 #[derive(Debug)]
@@ -11,4 +12,40 @@ pub struct Subtitle {
     pub end_time: Time,
     /// The subtitle itself
     pub text: String,
+}
+
+impl fmt::Display for Subtitle {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            out,
+            "{}\n{}-->{}\n{}",
+            self.pos, self.start_time, self.end_time, self.text
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn display() {
+        let subtitle = Subtitle {
+            pos: 1,
+            start_time: Time {
+                hours: 0,
+                minutes: 0,
+                seconds: 5,
+                milliseconds: 200,
+            },
+            end_time: Time {
+                hours: 0,
+                minutes: 0,
+                seconds: 6,
+                milliseconds: 300,
+            },
+            text: String::from("test"),
+        };
+        assert_eq!(subtitle.to_string(), "1\n00:00:05,200-->00:00:06,300\ntest");
+    }
 }
